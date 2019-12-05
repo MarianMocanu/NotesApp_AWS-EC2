8741edb666735e208bc309f4266b6725be329972
Marian Mocanu <gheo0174@stud.kea.dk>
2019-12-05 10:00:26 +0100

    2nd commit

--- src/main/java/dk/stud/kea/assignments/mandatory/MandatoryApplication.java
+++ src/main/java/dk/stud/kea/assignments/mandatory/MandatoryApplication.java
@@ -2,12 +2,19 @@ package dk.stud.kea.assignments.mandatory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MandatoryApplication {
+public class MandatoryApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(MandatoryApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(MandatoryApplication.class);
   }
 
 }
